c596672188dfa9077efe628eb2d099a0d9ed3d76
jonas meng <jonas.meng.jm@gmail.com>
2015-09-10 01:47:07 +0200

    maximal spg test passed

--- spg.cpp
+++ spg.cpp
@@ -9,15 +9,31 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 
 using namespace std;
 using namespace rapidjson;
 
 typedef set <int> edgeset;
-typedef map <set <int>, set < set <int> > > termspgset;
+typedef vector < set <int> > graph;
+typedef map <set <int>, set < vector< set<int> > > > termspgset;
+
+#define EDGE 1
+#define NODE 0
 
 //#define SPGDETECTIONDEBUG
-//#define PROGRESSCHECKING 
+#define PROGRESSCHECKING 
+//#define MAXIMALSPG
+
+void printgraph(const graph & g) {
+	cerr << "edge: ";
+	for (set<int>::iterator it = g[EDGE].begin(); it != g[EDGE].end(); it++)
+		cerr << *it << " ";
+	cerr << " node: ";
+	for (set<int>::iterator it = g[NODE].begin(); it != g[NODE].end(); it++)
+		cerr << *it << " ";
+	cerr << endl;
+}
 
 bool spgdetection(Document &document) {
 	/*
@@ -125,26 +141,49 @@ void serialcomb(termspgset &oldset, termspgset &newset,
 				set<int> key;
 				key.insert(p[i]);
 				key.insert(*itr);
-				set< set <int> > *oldspg = &oldset[key];
+				set<graph > *oldspg = &oldset[key];
 				key.erase(p[i]);
 				key.insert(p[(i+1)%2]);
-				vector<int> tmp;
+				graph tmp;
+				vector<int> stmp;
 				// enumerating all possible combination of SPGs from new and old SPG sets
-				for (set< set <int> >::iterator oldit = oldspg->begin();
+				for (set<graph>::iterator oldit = oldspg->begin();
 						oldit != oldspg->end(); oldit++) {
-					for (set< set <int> >::iterator newit = (it->second).begin();
+					for (set<graph>::iterator newit = (it->second).begin();
 							newit != (it->second).end(); newit++) {
-						set_intersection(newit->begin(), newit->end(),
-								oldit->begin(), oldit->end(), tmp.begin());
+						// std::back_inserter is necessary because no memory is allocated to
+						// vector tmp right now
+						stmp.clear();
+						set_intersection((*newit)[NODE].begin(), (*newit)[NODE].end(),
+								(*oldit)[NODE].begin(), (*oldit)[NODE].end(),
+								back_inserter(stmp));
 						// if two SPGs contain no same arcs, then perform serial combination
-						if (tmp.size() == 0) {
-							set_union(newit->begin(), newit->end(),
-									oldit->begin(), oldit->end(), tmp.begin());
-							intermidateset[key].insert(set<int>(tmp.begin(), tmp.end()));		
-							terminals[*itr].insert(p[(i+1)%2]);
-							terminals[p[(i+1)%2]].insert(*itr);
+						if (stmp.size() <= 1) { // serial combination should only has one single shared terminal
+							stmp.clear();
+							tmp.clear();
+							set_union((*newit)[NODE].begin(), (*newit)[NODE].end(),
+									(*oldit)[NODE].begin(), (*oldit)[NODE].end(),
+									back_inserter(stmp));
+							tmp.push_back(set<int>(stmp.begin(), stmp.end())); // push node set into graph representation
+							stmp.clear();
+							set_union((*newit)[EDGE].begin(), (*newit)[EDGE].end(),
+									(*oldit)[EDGE].begin(), (*oldit)[EDGE].end(),
+									back_inserter(stmp));
+							tmp.push_back(set<int>(stmp.begin(), stmp.end())); // push edge set into graph representation
+#ifdef MAXIMALSPG
+							cerr << "-------------serial----------------" << endl;
+							printgraph(*newit);
+							printgraph(*oldit);
+							cerr << "->" << endl;
+							printgraph(tmp);
+#endif
+							if (oldset[key].find(tmp) == oldset[key].end() &&
+									newset[key].find(tmp) == newset[key].end()) {
+								intermidateset[key].insert(tmp);	
+								terminals[p[i]].insert(p[(i+1)%2]);
+								terminals[p[(i+1)%2]].insert(p[i]);
+							}
 						}
-						tmp.clear();
 					}
 				}
 			}		
@@ -160,25 +199,46 @@ void parallelcomb(termspgset &oldset, termspgset &newset,
 		set<int> key = it->first;
 		if (oldset.find(key) == oldset.end())
 			continue;
-		set< set <int> >& newspg = it->second;
-		vector<int> tmp;
+		set<graph>& newspg = it->second;
+		graph tmp;
+		vector<int> stmp;
 		// try to combine all SPGs with same terminals
-		for (set< set<int> >::iterator oldit = oldset[key].begin();
+		for (set<graph>::iterator oldit = oldset[key].begin();
 				oldit != oldset[key].end(); oldit++) {
-			for (set< set <int> >::iterator newit = newspg.begin();
+			for (set<graph>::iterator newit = newspg.begin();
 					newit != newspg.end(); newit++) {
-				set_intersection(newit->begin(), newit->end(),
-						oldit->begin(), oldit->end(), tmp.begin());
+				stmp.clear();
+				set_intersection((*newit)[NODE].begin(), (*newit)[NODE].end(),
+						(*oldit)[NODE].begin(), (*oldit)[NODE].end(), 
+						back_inserter(stmp));
 				// if two SPGs contain no same arcs, then perform serial combination
-				if (tmp.size() == 0) {
+				if (stmp.size() <= 2) { // for parallel combination, there are at most 2
+					stmp.clear();
+					tmp.clear();
+					set_union((*newit)[NODE].begin(), (*newit)[NODE].end(),
+							(*oldit)[NODE].begin(), (*oldit)[NODE].end(),
+							back_inserter(stmp));
+					tmp.push_back(set<int>(stmp.begin(), stmp.end())); // push node set into graph represetnation
+					stmp.clear();
 					// because size of result of set union definitely larger than
 					// size of result of set intersection, which leads to overwrite of last result
 					// therefore set clear operation is ignored
-					set_union(newit->begin(), newit->end(),
-							oldit->begin(), oldit->end(), tmp.begin());
-					intermidateset[key].insert(set<int>(tmp.begin(), tmp.end()));		
+					// c++ union operation only add same element once
+					set_union((*newit)[EDGE].begin(), (*newit)[EDGE].end(),
+							(*oldit)[EDGE].begin(), (*oldit)[EDGE].end(),
+							back_inserter(stmp));
+					tmp.push_back(set<int>(stmp.begin(), stmp.end())); // push edge set into graph represetnation
+#ifdef MAXIMALSPG
+					cerr << "--------------parallel--------------------" << endl;
+					printgraph(*newit);
+					printgraph(*oldit);
+					cerr << "->" << endl;
+					printgraph(tmp);
+#endif
+					if (oldset[key].find(tmp) == oldset[key].end() &&
+							newset[key].find(tmp) == newset[key].end())
+						intermidateset[key].insert(tmp);
 				}
-				tmp.clear();
 			}
 		}	
 	}
@@ -186,33 +246,35 @@ void parallelcomb(termspgset &oldset, termspgset &newset,
 
 bool iteration(termspgset &oldset, termspgset &newset,
 		termspgset &intermidateset, map <int, set <int> > &terminals) {
-	//serialcomb(oldset, newset, intermidateset, terminals);
+	serialcomb(oldset, newset, intermidateset, terminals);
 	parallelcomb(oldset, newset, intermidateset);	
 	// merge old and new set
 	for (termspgset::iterator newit = newset.begin();
 			newit != newset.end(); ++newit) {
 		// set difference makes sure that all SPGs in vector are not included in old set
-		vector< set<int> > tmp; 
+		vector<graph> tmp; 
 		const set<int> &key = newit->first;
 		// following iterator providing const values which are read-only
 		// therefore no way to compare and copy
 		// possible solution is to write a three set operation manually
-		set_difference(newset[key].begin(), 
-				newset[key].end(),
-				oldset[key].begin(),
-				oldset[key].end(),
-				tmp.begin());
-		oldset[key].insert(tmp.begin(), tmp.end());
 		tmp.clear();
+		set_difference(newset[key].begin(), newset[key].end(),
+				oldset[key].begin(), oldset[key].end(),
+				back_inserter(tmp));
+		oldset[key].insert(tmp.begin(), tmp.end());
 	}
-	// copy intermidate set to new set
-	newset = intermidateset;
-	// clear intermidate set for next iteration
-	intermidateset.clear();
+	bool isLoop = false;
+	if (intermidateset.size() > 0) {
+		// copy intermidate set to new set
+		newset = intermidateset;
+		// clear intermidate set for next iteration
+		intermidateset.clear();
+		isLoop = true;
+	}
 	
 	// if new set is non-empty, then return true to continue iteration
 	// otherwise return false to stop
-	return (newset.size() > 0);
+	return isLoop;
 }
 
 void enumeratingspg(Document &document) {
@@ -224,22 +286,66 @@ void enumeratingspg(Document &document) {
 	 * */
 	map <int, set <int> > edgelabel;
 	map <int, set <int> > terminals;
+	set <int> edges;
 	termspgset oldset, newset, intermidateset;
 	int sz = document["bond"]["aid1"].Size(), a, b;
 	for (int idx = 0; idx < sz; idx++) {
+		edges.insert(idx);
 		a = document["bond"]["aid1"][idx].GetInt();
 		b = document["bond"]["aid2"][idx].GetInt();
 		terminals[a].insert(b);
 		terminals[b].insert(a);
 		edgelabel[idx].insert(a);
 		edgelabel[idx].insert(b);
-		set <int> tmp;
-		tmp.insert(idx);
+		// create a spg with both edge and node set
+		set <int> edge;
+		edge.insert(idx);
+		set <int> node;
+		node.insert(a);
+		node.insert(b);
+		vector <set <int> > tmp;
+		tmp.push_back(node);
+		tmp.push_back(edge);
+		// insert spg into old and new set
 		oldset[edgelabel[idx]].insert(tmp);
 		newset[edgelabel[idx]].insert(tmp);
 	}
 	
-	while (iteration(oldset, newset, intermidateset, terminals));
+	while (iteration(oldset, newset, intermidateset, terminals))
+		;
+
+	// identify maximal sub-SPG and corresponding violator set
+	int sz_max = 0;
+	set<graph>::iterator maxitr;
+	for (termspgset::iterator it = oldset.begin();
+			it != oldset.end(); it++) {
+		for (set<graph>::iterator ssit = (it->second).begin();
+				ssit != (it->second).end(); ++ssit) {
+			if ((*ssit)[EDGE].size() > sz_max) {
+				sz_max = (*ssit)[EDGE].size();
+				maxitr = ssit;
+			}
+		}
+	}
+	
+	vector<int> violator;
+	set<int>::iterator it = (*maxitr)[EDGE].begin();
+	cout << "," << sz_max <<",\"" << *it;
+	for (it++; it != (*maxitr)[EDGE].end(); it++)
+		cout << "," << *it;
+	int sz_vio = document["bond"]["aid1"].Size() - sz_max;
+	set_difference(edges.begin(), edges.end(),
+			(*maxitr)[EDGE].begin(), (*maxitr)[EDGE].end(),
+			back_inserter(violator));
+	if (violator.size() > 0) {
+		vector<int>::iterator vioit = violator.begin();
+		cout << "\"," << sz_vio << ",\"" << *vioit;
+		for (vioit++; vioit != violator.end(); vioit++)
+			cout << "," << *vioit;
+		cout << "\"" << endl;
+	} else {
+		cout << "\",0,\"\"" << endl;
+	}
 }
 
 int main(int argc, char **argv) {
@@ -253,29 +359,40 @@ int main(int argc, char **argv) {
 	int cnt = 0, cntspg = 0;
 	while (inputfile.getline(mol, size)) {
 		cnt++;
-		/*
-		if (cnt % 2000000 == 0) {			
+		if (cnt % 20000 == 0) {			
 			fprintf(stderr, "\r%.2f", (cnt*100.0/2000000));
 		}
-		*/
 #ifdef PROGRESSCHECKING
-		printf("%d | %s\n", cnt, mol);
+		fprintf(stderr, "%d | %s\n", cnt, mol);
 #endif
 		// get single molecule in memory
 		document.Parse(mol);
+		cout << "id,sz_max,maximal,sz_vio,violator" << endl;
+		cout << document["id"].GetString();
+		// omit graphs whose size is less than 0 or larger than 60
+		if(document["bond"]["aid1"].Size() <= 0 ||
+				document["bond"]["aid1"].Size() > 60) {
+			cout << ",0,\"\",0,\"\"" << endl;
+			continue;
+		}
 		if(spgdetection(document)) {
 			cntspg++;
 #ifdef PROGRESSCHECKING
-			printf("%d is SPG\n", cnt);
+			fprintf(stderr, "%d is SPG\n", cnt);
 #endif
+			cout << "," << document["bond"]["aid1"].Size() << ",\"0";
+			for (int i = 1; i < document["bond"]["aid1"].Size(); i++)
+				cout << "," << i;
+			cout << "\",0,\"\"" << endl;
 		} else {
-			enumeratingspg(document);
 #ifdef PROGRESSCHECKING
-			printf("%d is not SPG\n", cnt);
+			fprintf(stderr, "%d is not SPG\n", cnt);
 #endif
+			enumeratingspg(document);
 		}
 	}
-	printf("total number = %d, number of spg = %d, percentage = %.2f\n", cnt, cntspg, cntspg * 100.0 / cnt);
+	// print information regarding percentage of SPG in database
+	//printf("total number = %d, number of spg = %d, percentage = %.2f\n", cnt, cntspg, cntspg * 100.0 / cnt);
 
 	//#pragma omp parallel for
 	
